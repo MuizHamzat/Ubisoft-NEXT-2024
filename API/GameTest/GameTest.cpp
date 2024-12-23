6f229d8d853a30cfb8dab35ca7f552f42d928801
MuizHamzat <muizhamzat@gmail.com>
2024-12-23 13:33:33 -0500

    Sprite is drawn to screen, and movement is implemented. Sprite is also deleted on Shutdown.

--- API/GameTest/GameTest.cpp
+++ API/GameTest/GameTest.cpp
@@ -9,23 +9,52 @@
 #include <math.h>  
 //------------------------------------------------------------------------
 #include "app\app.h"
+#include "player.h"
+//------------------------------------------------------------------------
+//Definitions
+CSimpleSprite* sprite;
 //------------------------------------------------------------------------
 void Init()
 {
-
+	sprite = App::CreateSprite(".\\TestData\\ship.bmp", 1, 1);
+	sprite->SetPosition(400.0f, 400.0f);
 }
 
 void Update(float deltaTime)
 {
-	
+	sprite->Update(deltaTime);
+	if (App::GetController().GetLeftThumbStickX() > 0.5f)
+	{
+		float x, y;
+		sprite->GetPosition(x, y);
+		sprite->SetPosition(x + 1.0f, y);
+	}
+	if (App::GetController().GetLeftThumbStickX() < -0.5f)
+	{
+		float x, y;
+		sprite->GetPosition(x, y);
+		sprite->SetPosition(x - 1.0f, y);
+	}
+	if (App::GetController().GetLeftThumbStickY() > 0.5f)
+	{
+		float x, y;
+		sprite->GetPosition(x, y);
+		sprite->SetPosition(x, y + 1.0f);
+	}
+	if (App::GetController().GetLeftThumbStickY() < -0.5f)
+	{
+		float x, y;
+		sprite->GetPosition(x, y);
+		sprite->SetPosition(x, y - 1.0f);
+	}
 }
 
 void Render()
 {
-
+	sprite->Draw();
 }
 
 void Shutdown()
 {
-
+	delete sprite;
 }
